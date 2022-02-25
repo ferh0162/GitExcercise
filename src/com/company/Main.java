455ba96dcafca7f323a187e9d262d10105120f3d
ferh0162 <ferh0162@stud.kea.dk>
2022-02-25 15:15:36 +0100

    en nyere kode

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("En ny kode");
+        System.out.println("En endnu nyere kode");
         System.out.println("Koden");
+        System.out.println("Hvad laver du inde på min github din lille...");
     }
 }
