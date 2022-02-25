522f26d6bf1195659a6073a2b4d3655a6016462d
ferh0162 <ferh0162@stud.kea.dk>
2022-02-25 14:32:18 +0100

    En ny kode

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hej");
+        System.out.println("En ny kode");
+        System.out.println("Koden");
     }
 }
